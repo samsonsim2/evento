ac12c174297abe44708b2e7f9dfeb8c01da7d564
s.sim <s.sim@esrisingapore.com.sg>
2023-12-28 09:17:00 +0800

    testing

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      event
-    </div>
-  )
-}
+  return <div>eventdsada</div>;
+};
 
-export default page
+export default page;
